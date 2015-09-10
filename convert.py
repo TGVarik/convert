1ec1921527aaf6cf65a306e32e661308bcb0ed59
Tom G Varik <tom.v@riks.io>
2015-09-09 23:55:28 -0700

    sort to-do files

--- convert.py
+++ convert.py
@@ -18,10 +18,11 @@ def blu_movies(folder):
   files = []
   for root, dirs, fs in os.walk(folder):
     files.extend([os.path.join(root, f) for f in fs if
-                  os.path.splitext(f)[1].lower() in ['.mkv', '.mp4', '.avi']])
+                  os.path.splitext(f)[1].lower() in ['.mkv', '.mp4', '.avi'] and
+                  searcher.search(os.path.splitext(os.path.basename(f))[0]) is not None])
   print('{:d} files found.'.format(len(files)))
 
-  for f in files:
+  for f in sorted(files, key=lambda f: int(searcher.search(os.path.splitext(os.path.basename(f))[0]).group('tmdb_id'))):
     match = searcher.search(os.path.splitext(os.path.basename(f))[0])
     if match:
       process_movie(f,
@@ -127,5 +128,4 @@ if __name__ == '__main__':
 
   setup_logging('convert')
   log = getLogger()
-  blu_movies('/tank/Incoming/Cloud_Atlas')
   blu_movies('/tank/Incoming')
