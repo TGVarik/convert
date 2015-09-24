5c885a0f60b595d4c9185102115d7c002ec1b0af
Tom G Varik <tom.v@riks.io>
2015-09-24 02:11:21 -0700

    debug

--- subs.py
+++ subs.py
@@ -7,10 +7,11 @@ for root, dirs, fs in os.walk('/tank/Incoming'):
   files.extend([os.path.join(root, f) for f in fs if
                 os.path.splitext(f)[1].lower() in ['.mkv', '.done']])
 for f in files:
+  print(f);
   ffprobe = get_ffprobe(f)
   sub_streams = [s for s in ffprobe['streams'] if s['codec_type'] == 'subtitle' and s['codec_name'] in ['pgssub', 'dvdsub']]
   if len(sub_streams) > 0:
-    cmd = ['ffmpeg', '-hide_banner', '-stats', '-y', '-v', 'quiet', '-i', f]
+    cmd = ['ffmpeg', '-hide_banner', '-y', '-v', 'quiet', '-i', f]
     maps = []
     codecs = []
     sub_index = -1
